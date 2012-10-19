802d5c746c77db8c5ccedb22574d0d6dcb4e2619
Joseph GIlley <joe.gilley@gmail.com>
2012-10-19 17:09:38 -0400

    Made more generic for extendables.

--- Whut/src/com/flipflop/game/whut/GameComponent.java
+++ Whut/src/com/flipflop/game/whut/GameComponent.java
@@ -6,19 +6,29 @@ import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.GraphicsConfiguration;
+import java.awt.GraphicsEnvironment;
+import java.awt.RenderingHints;
 import java.awt.image.BufferStrategy;
+import java.awt.image.VolatileImage;
 
-public abstract class GameComponent extends Canvas implements Runnable{
+public abstract class GameComponent extends Canvas implements Runnable {
 	private static final long serialVersionUID = 7896446093560185240L;
+	private static final int DEFAULT_FPS = 60;
 	protected Container parent = null;
 	protected World world = null;
 	protected InputManager im = null;
 	protected BufferStrategy bufferStrategy = null;
-	private Dimension gameSize = new Dimension(500,500);
+	protected VolatileImage volatileBuffer = null;
+	protected GraphicsConfiguration gc = null;
+	private Dimension gameSize = new Dimension(500, 500);
 	protected boolean gameRunning = false;
-	
+
 	public GameComponent(Container parent) {
 		this.parent = parent;
+		this.gc = GraphicsEnvironment.getLocalGraphicsEnvironment()
+				.getDefaultScreenDevice()
+				.getDefaultConfiguration();
 		this.im = new InputManager(this.parent);
 	}
 
@@ -34,73 +44,91 @@ public abstract class GameComponent extends Canvas implements Runnable{
 	public abstract void update(long tm);
 
 	/**
-	 * Initialize any components the game has.  <code>.init()</code> must be called <strong>after</strong> the owning window has been made visible.
+	 * Initialize any components the game has. <code>.init()</code> must be
+	 * called <strong>after</strong> the owning window has been made visible.
 	 */
 	public void init() {
 		this.parent.setIgnoreRepaint(true);
-		this.createBufferStrategy(2);
+		this.setIgnoreRepaint(true);
+		this.createBufferStrategy(1);
 		this.bufferStrategy = this.getBufferStrategy();
-		System.out.println("Using " + this.bufferStrategy.getClass().getSimpleName());
+		this.volatileBuffer = this.gc.createCompatibleVolatileImage(
+				this.getWidth(), this.getHeight());
+		
+		System.setProperty("sun.java2d.opengl", "true");
+		System.setProperty("apple.awt.graphics.UseQuartz","true");
+		System.setProperty("apple.awt.graphics.EnableQ2DX","true");
 	}
+
 	/**
-	 * Let the game know that execution has begun.  Let the game take any necessary pre-cautions.
+	 * Let the game know that execution has begun. Let the game take any
+	 * necessary pre-cautions.
 	 */
 	public abstract void prepare();
 
 	/**
-	 * Let the game know that execution has stopped.  Let the game take any necessary pre-cautions.
+	 * Let the game know that execution has stopped. Let the game take any
+	 * necessary pre-cautions.
 	 */
 	public abstract void stop();
-	
+
 	/**
-	 * The entry point of the game.  This is the game loop.  Override this method to fashion your own game loop.  
+	 * The entry point of the game. This is the game loop. Override this method
+	 * to fashion your own game loop.
 	 */
 	@Override
 	public void run() {
 		Graphics2D g = null;
 		long lastTime = System.currentTimeMillis();
 		long updateFPS = System.currentTimeMillis();
+
 		this.gameRunning = true;
-		
+
 		while (this.gameRunning) {
 			g = (Graphics2D) this.bufferStrategy.getDrawGraphics();
-			
+			g.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION, RenderingHints.VALUE_ALPHA_INTERPOLATION_SPEED);
+			g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_OFF);
+			g.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING, RenderingHints.VALUE_COLOR_RENDER_SPEED);
+			g.setRenderingHint(RenderingHints.KEY_DITHERING, RenderingHints.VALUE_DITHER_DISABLE);
+			g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_NEAREST_NEIGHBOR);
+			g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_SPEED);
 			long delta = System.currentTimeMillis() - lastTime;
-			if (delta > (1000/65)) {
+			if (delta > (1000 / DEFAULT_FPS)) {
 				lastTime = System.currentTimeMillis();
 				g.setColor(Color.black);
 				g.setBackground(Color.gray);
-				g.clearRect(0, 0, this.getWidth(), this.getHeight());
+				g.clearRect(0, 0, this.getWidth(),
+						this.getHeight());
 				this.update(delta);
 				this.render(g);
-				if (System.currentTimeMillis() - updateFPS >= 1000) {
-					System.out.println("FPS:"+(1000/delta));
+				if (System.currentTimeMillis() - updateFPS >= 5000) {
+					System.out.println("FPS:" + (1000 / delta));
 					updateFPS = System.currentTimeMillis();
 				}
-				
 			} else {
 				try {
 					Thread.sleep(5, 0);
-				} catch (InterruptedException e) {}
+				} catch (InterruptedException e) {
+				}
 			}
-			
-			g.dispose();
+
 			this.bufferStrategy.show();
 		}
 	}
-	
+
 	public void setGameSize(int width, int height) {
 		this.gameSize = new Dimension(width, height);
 	}
+
 	public void setGameSize(Dimension d) {
 		this.gameSize = d;
 	}
-	
+
 	@Override
 	public Dimension getPreferredSize() {
 		return this.gameSize;
 	}
-	
+
 	public void changeWorld(World world) {
 		this.world.stop();
 		this.world = world;
